89704870ae6442158d3d7c6ee1efa7561746683d
qiaozhiyuan <18226851365@163.com>
2021-04-19 17:31:42 +0800

    修改-登录方式对接数据登录等

--- miniprogram-1/pages/login/login.js
+++ miniprogram-1/pages/login/login.js
@@ -8,7 +8,7 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
+    canIUseGetUserProfile: true,
     canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
     signature: '',
     rawData: '',
@@ -29,28 +29,69 @@ Page({
     // }).catch((error) => {
     //   console.log(error)
     // })
-    wx.getUserProfile({
-      desc: '获取用户信息登录', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+    wx.getUserInfo({
       success: (res) => {
         console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true,
-          signature: res.signature,
-          rawData: res.rawData,
-          encryptedData: res.encryptedData,
-          iv: res.iv
+        wx.getUserProfile({
+          desc: '获取用户信息登录', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+          success: (res) => {
+            console.log(res)
+          }
         })
+        // this.setData({
+        //   userInfo: res.userInfo,
+        //   hasUserInfo: true,
+        //   signature: res.signature,
+        //   rawData: res.rawData,
+        //   encryptedData: res.encryptedData,
+        //   iv: res.iv
+        // })
         // 微信一键登录
-        this.wxKeyLogin();
+        // this.wxKeyLogin();
+            Util.Request({
+              method: 'POST',
+              url: app.globalData.test + "/wxApp/oneKeyLogin/1.0",
+              data: {
+                code: app.globalData.wxCode,
+                // signature: this.data.signature,
+                // rawData: this.data.rawData,
+                encryptedData: res.encryptedData,
+                iv: res.iv
+              },
+            }).then(res => {
+              console.log(res)
+            }).catch((error) => {
+              console.log(error);
+            })
+
       }
     })
+    
   },
+  // 获取用户手机号码
+  // getPhoneNumber: (res) => {
+  //   console.log(res)
+  //   Util.Request({
+  //     method: 'POST',
+  //     url: app.globalData.test + "/wxApp/oneKeyLogin/1.0",
+  //     data: {
+  //       code: app.globalData.wxCode,
+  //       // signature: this.data.signature,
+  //       // rawData: this.data.rawData,
+  //       encryptedData: res.detail.encryptedData,
+  //       iv: res.detail.iv
+  //     },
+  //   }).then(res => {
+  //     console.log(res)
+  //   }).catch((error) => {
+  //     console.log(error);
+  //   })
+  // },
   // 一键登录方法
-  wxKeyLogin() {
+  wxKeyLogin(res) {
     Util.Request({
       method: 'POST',
-      url: app.globalData.test + "/wxApp/oneKeyLogin",
+      url: app.globalData.test + "/wxApp/oneKeyLogin/1.0",
       data: {
         code: app.globalData.wxCode,
         // signature: this.data.signature,
